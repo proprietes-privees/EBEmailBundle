bf1315ab21e59539276b0c059fa0266933a756bc
Emmanuel BALLERY <emmanuel.ballery@gmail.com>
2016-03-14 13:28:19 +0100

    Cleaned configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,6 +3,7 @@
 namespace EB\EmailBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -34,15 +35,11 @@ class Configuration implements ConfigurationInterface
         $senders->scalarNode('name')->isRequired()->cannotBeEmpty()->info('Sender name')->example('John Doe');
         $senders->scalarNode('email')->isRequired()->cannotBeEmpty()->info('Sender email')->example('john.doe@gmail.com');
 
-        // Resources to add for all emails
-        $children->arrayNode('globals')->useAttributeAsKey('name')->info('Global to add in each template')->example('A parameter')->prototype('scalar');
-        $children->arrayNode('images')->useAttributeAsKey('name')->info('Images to attach inline')->example('/path/to/file')->prototype('scalar');
-        $children->arrayNode('attachments')->info('Files to attach')->example('/path/to/file')->prototype('scalar');
-
-        // Static recipients
-        $recipients = $children->arrayNode('recipients')->addDefaultsIfNotSet()->children();
-        $recipients->scalarNode('name')->isRequired()->cannotBeEmpty()->info('Recipient name')->example('John Doe');
-        $recipients->scalarNode('email')->isRequired()->cannotBeEmpty()->info('Recipient email')->example('john.doe@gmail.com');
+        // Resources to add to all emails
+        $this->addGlobalsChildrenConfiguration($children);
+        $this->addImagesChildrenConfiguration($children);
+        $this->addAttachmentsChildrenConfiguration($children);
+        $this->addRecipientsChildrenConfiguration($children);
 
         /** @var ArrayNodeDefinition $emailsNode */
         $emailsNode = $children
@@ -55,15 +52,79 @@ class Configuration implements ConfigurationInterface
         $email->scalarNode('text_template')->isRequired()->cannotBeEmpty()->info('Text Template')->example('AcmeDefautBundle::_text_email.html.twig');
         $email->scalarNode('html_template')->defaultNull()->info('HTML Template')->example('AcmeDefautBundle::_html_email.html.twig');
         $email->scalarNode('subject')->isRequired()->cannotBeEmpty()->info('Email subject')->example('Welcome {{user.username}} to {{app_name}} !');
-        $email->arrayNode('globals')->useAttributeAsKey('name')->info('Global to add in this template')->example('A parameter')->prototype('scalar');
-        $email->arrayNode('images')->useAttributeAsKey('name')->info('Images to attach inline')->example('/path/to/file')->prototype('scalar');
-        $email->arrayNode('attachments')->info('Files to attach')->example('/path/to/file')->prototype('scalar');
-
-        // Static email recipients
-        $emailRecipients = $email->arrayNode('recipients')->addDefaultsIfNotSet()->children();
-        $emailRecipients->scalarNode('name')->isRequired()->cannotBeEmpty()->info('Recipient name')->example('John Doe');
-        $emailRecipients->scalarNode('email')->isRequired()->cannotBeEmpty()->info('Recipient email')->example('john.doe@gmail.com');
+        $this->addGlobalsChildrenConfiguration($email);
+        $this->addImagesChildrenConfiguration($email);
+        $this->addAttachmentsChildrenConfiguration($email);
+        $this->addRecipientsChildrenConfiguration($email);
 
         return $tb;
+    }
+
+    /**
+     * Add globals children configuration
+     *
+     * @param NodeBuilder $children
+     */
+    private function addGlobalsChildrenConfiguration(NodeBuilder $children)
+    {
+        $children
+            ->arrayNode('globals')
+            ->useAttributeAsKey('name')
+            ->info('Global to add in this template')
+            ->example('A parameter')
+            ->prototype('scalar');
+    }
+
+    /**
+     * Add images children configuration
+     *
+     * @param NodeBuilder $children
+     */
+    private function addImagesChildrenConfiguration(NodeBuilder $children)
+    {
+        $children
+            ->arrayNode('images')
+            ->useAttributeAsKey('name')
+            ->info('Images to attach inline')
+            ->example('/path/to/file')
+            ->prototype('scalar');
+    }
+
+    /**
+     * Add attachments children configuration
+     *
+     * @param NodeBuilder $children
+     */
+    private function addAttachmentsChildrenConfiguration(NodeBuilder $children)
+    {
+        $children
+            ->arrayNode('attachments')
+            ->info('Files to attach')
+            ->example('/path/to/file')
+            ->prototype('scalar');
+    }
+
+    /**
+     * Add recipients children configuration
+     *
+     * @param NodeBuilder $children
+     */
+    private function addRecipientsChildrenConfiguration(NodeBuilder $children)
+    {
+        $recipients = $children->arrayNode('recipients')->addDefaultsIfNotSet()->children();
+
+        $recipients
+            ->scalarNode('name')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->info('Recipient name')
+            ->example('John Doe');
+
+        $recipients
+            ->scalarNode('email')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->info('Recipient email')
+            ->example('john.doe@gmail.com');
     }
 }
